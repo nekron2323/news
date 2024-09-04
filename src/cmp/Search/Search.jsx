40fda56487eed7914cdc14fe853e4a02f979d651
nekron2323 <goncharov.uv@gmail.com>
2024-09-04 12:30:26 +0300

    add search

---
+++ src/cmp/Search/Search.jsx
@@ -0,0 +1,16 @@
+import styles from './styles.module.css'
+
+const Search = ({ keywords, setKeywords }) => {
+    return (
+        <div className={styles.search}>
+            <input
+                type='text'
+                value={keywords}
+                onChange={e => setKeywords(e.target.value)}
+                className={styles.input} 
+                placeholder='Input keyword'/>
+        </div>
+    )
+}
+
+export default Search
