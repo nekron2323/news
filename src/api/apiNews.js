fc798f6d5355e38ef6b6f1b2d602d25d49f960c4
nekron2323 <goncharov.uv@gmail.com>
2024-09-04 11:27:51 +0300

    add categories

--- src/api/apiNews.js
+++ src/api/apiNews.js
@@ -3,16 +3,30 @@ import axios from "axios"
 const BASE_URL = import.meta.env.VITE_NEWS_BASE_API_URL
 const API_KEY = import.meta.env.VITE_NEWS_API_KEY
 
-export const getNews = async (page_number=1, page_size=10) => {
+export const getNews = async ({ page_number = 1, page_size = 10, category }) => {
     try {
         const res = await axios.get(`${BASE_URL}search`, {
             params: {
                 apiKey: API_KEY,
                 page_number,
-                page_size
+                page_size,
+                category
             }
         })
         return res.data?.news
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+export const getCategories = async () => {
+    try {
+        const res = await axios.get(`${BASE_URL}available/categories`, {
+            params: {
+                apiKey: API_KEY
+            }
+        })
+        return res.data?.categories
     } catch (error) {
         console.log(error)
     }
