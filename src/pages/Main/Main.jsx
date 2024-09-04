cfc9c7194d7bb11866b139cbb1683a49b1f288d1
nekron2323 <goncharov.uv@gmail.com>
2024-09-04 09:16:28 +0300

    add skeleton

--- src/pages/Main/Main.jsx
+++ src/pages/Main/Main.jsx
@@ -3,13 +3,19 @@ import NewsBanner from '../../cmp/NewsBanner/NewsBanner'
 import { useEffect, useState } from 'react'
 import { getNews } from '../../api/apiNews'
 import NewList from '../../cmp/NewList/NewList'
+import Skeleton from '../../cmp/Skeleton/Skeleton'
 
 const Main = () => {
     const [news, setNews] = useState([])
+    const [isLoading, setIsLoading] = useState(true)
+
     useEffect(_ => {
         const fetchNews = async () => {
             try {
+                setIsLoading(true)
                 setNews(await getNews())
+                setIsLoading(false)     
+                           
             } catch (error) {
                 console.log(error)
             }
@@ -19,8 +25,8 @@ const Main = () => {
 
     return (
         <main className={styles.main}>
-            {news.length ? <NewsBanner item={news[0]} /> : null}
-            <NewList news={news} />
+            {news.length && !isLoading ? <NewsBanner item={news[0]} /> : <Skeleton type='banner' />}
+            {!isLoading ? <NewList news={news} /> : <Skeleton type='item' count={10} />}
         </main>
     )
 }
