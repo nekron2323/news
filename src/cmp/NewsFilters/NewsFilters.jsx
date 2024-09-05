5b9f67b532ae91374b9857a822ab767fd4d3fe72
nekron2323 <goncharov.uv@gmail.com>
2024-09-05 09:12:45 +0300

    add slider

--- src/cmp/NewsFilters/NewsFilters.jsx
+++ src/cmp/NewsFilters/NewsFilters.jsx
@@ -3,15 +3,20 @@ import { getCategories } from '../../api/apiNews'
 import { useFetch } from '../../helpers/hooks/useFetch'
 import Categories from '../Categories/Categories'
 import Search from '../Search/Search'
+import Slider from '../Slider/Slider'
 
-const NewsFilters = ({filters, changeFilters}) => {
+const NewsFilters = ({ filters, changeFilters }) => {
     const { data: dataCategories } = useFetch(getCategories)
     return (
         <div className={styles.filters}>
-            {dataCategories?.length && <Categories
-                categories={dataCategories}
-                setSelectedCategory={(category) => changeFilters('category', category)}
-                selectedCategory={filters.category} />}
+            {dataCategories?.length &&
+                <Slider>
+                    <Categories
+                        categories={dataCategories}
+                        setSelectedCategory={(category) => changeFilters('category', category)}
+                        selectedCategory={filters.category} />
+                </Slider>
+            }
             <Search
                 keywords={filters.keywords}
                 setKeywords={(keywords) => changeFilters('keywords', keywords)} />
